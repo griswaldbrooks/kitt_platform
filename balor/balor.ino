74220f6939d2f9d89cd35f9717fb499f868ef96b
Kiran Mohan <kmohan@wpi.edu>
2018-05-13 20:34:07 -0700

    [#156980708] platform can ask for servo / motor calibration params (#2)
    * [#156980708] platform can ask for servo / motor calibration params

--- balor/balor.ino
+++ balor/balor.ino
@@ -1,44 +1,57 @@
 /**@file kitt_teensy_ros_node.ino */
 /*
  * Kitt Teensy ROS Node
- * 
+ *
  * This code is the ROS node that executes on the Teensy processor
  * for the Kitt Car team.
- * 
+ *
  * It uses the rosserial library to communicate with ROS. It uses
  * Arduino and Teensy libraries to interact with systems and
  * circuits connected to the Teensy processor. This code assumes
  * that a Teensy 3.5 is used.
- * 
+ *
  * https://github.com/kittcar
- * 
+ *
  */
 
 // ROS includes
 #include <ros.h>
 #include <std_msgs/Header.h>
 #include <std_msgs/Empty.h>
-#include <std_msgs/Int32.h>
+#include <std_msgs/String.h>
 #include <ackermann_msgs/AckermannDrive.h>
+
+// C++ includes
+#include <math.h>
 
 ros::NodeHandle ros_nh;
 
 std_msgs::Header heartbeat_msg;
 ros::Publisher heartbeat_pub("heartbeat", &heartbeat_msg);
 
-std_msgs::Int32 param_msg;
-ros::Publisher param_pub("param", &param_msg);
+std_msgs::String error_msg;
+ros::Publisher error_pub("platform/debug/error", &error_msg);
 
 ackermann_msgs::AckermannDrive debug_drive_msg;
-ros::Publisher debug_drive_pub("/platform/debug/drive", &debug_drive_msg);
+ros::Publisher debug_drive_pub("platform/debug/drive", &debug_drive_msg);
 
 void listenerCallback(const std_msgs::Empty& listener_msg);
 ros::Subscriber<std_msgs::Empty> listener_sub("listener", listenerCallback);
 
 void driveCallback(const ackermann_msgs::AckermannDrive& drive_msg);
-ros::Subscriber<ackermann_msgs::AckermannDrive> drive_sub("/platform/drive", driveCallback);
+ros::Subscriber<ackermann_msgs::AckermannDrive> drive_sub("platform/drive", driveCallback);
 
 const int LED_PIN = 13;
+
+// ROS params
+// steering offset in radians
+float steering_offset = M_PI / 2;
+// min_speed in m/s
+float min_speed = 0.0;
+// max_speed in m/s
+float max_speed = 5.0;
+
+bool params_retrieved_successfully = true;
 
 /**
  * @brief toggle LED state
@@ -68,11 +81,31 @@ void driveCallback(const ackermann_msgs::AckermannDrive& drive_msg)
   debug_drive_pub.publish(&debug_drive_msg);
 }
 
+/**
+ * @brief get all the required params from the ROS param server
+ */
+bool getRequiredParams()
+{
+  if (!ros_nh.getParam("drive/steering_offset", &steering_offset))
+  {
+    return false;
+  }
+  if(!ros_nh.getParam("drive/min_speed", &min_speed))
+  {
+    return false;
+  }
+  if(!ros_nh.getParam("drive/max_speed", &max_speed))
+  {
+    return false;
+  }
+  return true;
+}
+
 void setup()
 {
   ros_nh.advertise(heartbeat_pub);
-  ros_nh.advertise(param_pub);
   ros_nh.advertise(debug_drive_pub);
+  ros_nh.advertise(error_pub);
   ros_nh.subscribe(listener_sub);
   ros_nh.subscribe(drive_sub);
   ros_nh.getHardware()->setBaud(115200);
@@ -83,15 +116,7 @@ void setup()
   }
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
-  int tmp = 0;
-  if (ros_nh.getParam("tmp_param", &tmp))
-  {
-    param_msg.data = tmp;
-  }
-  else
-  {
-    param_msg.data = 9999;
-  }
+  params_retrieved_successfully = getRequiredParams();
 }
 
 void loop()
@@ -99,8 +124,11 @@ void loop()
   // Publish heartbeat_msg every 1 second
   heartbeat_msg.stamp = ros_nh.now();
   heartbeat_pub.publish(&heartbeat_msg);
-  param_pub.publish(&param_msg);
+  if (!params_retrieved_successfully)
+  {
+    error_msg.data = "Error retrieving params";
+    error_pub.publish(&error_msg);
+  }
   ros_nh.spinOnce();
   delay(1000);
 }
-
